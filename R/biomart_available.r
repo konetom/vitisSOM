caf4fa4d080d75486642de009eae1b16cade5f0e
konetom <tom4konecny@gmail.com>
2024-04-19 19:14:49 +0200

    20240419

--- R/biomart_available.r
+++
@@ -1,6 +0,0 @@
-biomart.available <- function(env)
-{
-  mart <- try({ suppressMessages({ useMart(biomart=env$preferences$database.biomart, host=env$preferences$database.host, verbose=FALSE) }) }, silent=TRUE)
-
-  return(is(mart,"Mart"))
-}
