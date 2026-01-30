adbe422f1a94ad529085aed8cde59156b30bbe51
konetom <tom4konecny@gmail.com>
2026-01-30 14:51:27 +0400

    test

--- R/detect_ensembl_dataset.r
+++ R/detect_ensembl_dataset.r
@@ -5,7 +5,7 @@ pipeline.detectEnsemblDataset <- function(env)
     mart <- useMart(biomart = "plants_mart", host = "https://plants.ensembl.org",
                     dataset = "vvinifera_eg_gene")
     biomart.table <- getBM(c("ensembl_gene_id", "chromosome_name",
-                             "name_1006", "definition_1006", "goslim_goa_description"), "ensembl_gene_id", rownames(env$indata),
+                             "name_1006", "definition_1006"), "ensembl_gene_id", rownames(env$indata),
                            mart, checkFilters = FALSE)
     env$preferences$database.dataset <- "vvinifera_eg_gene"
     env$preferences$database.id.type <- rownames(env$indata)
